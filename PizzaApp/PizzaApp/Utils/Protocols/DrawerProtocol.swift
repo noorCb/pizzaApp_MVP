299f6a8072802fd1843da078529ff21d5a4a4da4
noorCb <noor_cb@hotmail.com>
2023-04-10 17:39:54 +0200

    ADD: custom cell, protocols, storyboard update

---
+++ PizzaApp/PizzaApp/Utils/Protocols/DrawerProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  DrawerProtocol.swift
+//  PizzaApp
+//
+//  Created by Aisha Cabrera Bel on 10/04/2023.
+//  Copyright © 2023 Noor Cabrera Bel. All rights reserved.
+//
+
+import Foundation
---
+++ PizzaApp/PizzaApp/View/Cell/PizzaCell.swift
@@ -0,0 +1,9 @@
+//
+//  PizzaCell.swift
+//  PizzaApp
+//
+//  Created by Aisha Cabrera Bel on 10/04/2023.
+//  Copyright © 2023 Noor Cabrera Bel. All rights reserved.
+//
+
+import Foundation
